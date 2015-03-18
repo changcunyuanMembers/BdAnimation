412b55ccfcd488866fd874af318822cd496f08b0
zhangyihui02 <zhangyihui02@baidu.com>
2015-03-18 14:42:04 +0800

    first commit

---
+++ TestAnimation/TestAnimation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestAnimation
+//
+//  Created by apple  on 13-8-6.
+//  Copyright (c) 2013年 apple . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "CoreAnimationEffect.h"
+@interface ViewController : UIViewController
+{
+    UIImageView *imageView;
+}
+@end
---
+++ TestAnimation/TestAnimation/ViewController.m
@@ -0,0 +1,38 @@
+//
+//  ViewController.m
+//  TestAnimation
+//
+//  Created by apple  on 13-8-6.
+//  Copyright (c) 2013年 apple . All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 50, 300, 300)];
+    imageView.image = [UIImage imageNamed:@"3.jpg"];
+    imageView.contentMode = UIViewContentModeScaleAspectFit;
+    [self.view addSubview:imageView];
+    [imageView release];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event{
+    
+    [CoreAnimationEffect animationRotateAndScaleDownUp: imageView];
+}
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
